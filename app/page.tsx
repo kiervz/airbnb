d42335451b1dc0566d9f7561fbbd356bb6090a1b
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-05-25 23:49:34 +0800

    add client only for hydration

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import getListings, { IListingsParams } from "./actions/getListings";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
+import ClientOnly from "./components/ClientOnly";
 
 interface HomeProps {
   searchParams: IListingsParams
-}
+};
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
@@ -14,27 +15,29 @@ const Home = async ({ searchParams }: HomeProps) => {
 
   if (listings.length === 0) {
     return (
-      <>
+      <ClientOnly>
         <EmptyState showReset />
-      </>
+      </ClientOnly>
     );
   }
-  
+
   return (
-    <>
+    <ClientOnly>
       <Container>
-        <div className="
-          pt-24
-          grid
-          grid-cols-1
-          sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        ">
-          { listings.map((listing: any) => (
+        <div 
+          className='
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          '
+        >
+          {listings.map((listing: any) => (
             <ListingCard
               currentUser={currentUser}
               key={listing.id}
@@ -43,8 +46,8 @@ const Home = async ({ searchParams }: HomeProps) => {
           ))}
         </div>
       </Container>
-    </>
-  );
+    </ClientOnly>
+  )
 }
 
 export default Home;
